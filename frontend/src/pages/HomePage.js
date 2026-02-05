49fd8a662d6c6e789cfeea4b94a6d801980494d0
Loretta Columba <lorettaperaldi@gmail.com>
2026-02-05 14:49:03 +0100

    Replace image_H_7.jpg with image_H_7_new.jpg

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -236,7 +236,7 @@ const HomePage = () => {
               viewport={{ once: true }}
             >
               <img
-                src="/image_H_7.jpg"
+                src="/image_H_7_new.jpg"
                 alt="Romantic château moment"
                 className="w-full h-[450px] object-cover"
               />
